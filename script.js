27ab5cbdde92c6c4cd04b6270357dd8cc9adf91b
Elmaracudja <150439723+Elmaracudja@users.noreply.github.com>
2026-06-12 19:42:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let playlist = [];
+let currentIndex = 0;
+const video = document.getElementById("player");
 
+fetch("./playlist.json")
+  .then(res => res.json())
+  .then(data => {
+    playlist = data;
+    if (playlist.length > 0) {
+      video.src = playlist[0].src;
+    }
+  });
+
+video.addEventListener("ended", () => {
+  currentIndex = (currentIndex + 1) % playlist.length;
+  video.src = playlist[currentIndex].src;
+  video.play();
+});
